ce357ed1bd1f642a85ff3f46f90c3aeb4d28a26c
cal <callum.walley@nesi.org.nz>
2025-02-17 15:43:53 +1300

    Merge branch 'main' of https://github.com/nesi/nesi-mkdoc-template

--- mkdocs_hooks.py
+++ mkdocs_hooks.py
@@ -1,3 +1,10 @@
+"""
+mkdocs_hooks allows injection of variables into templating stage of rendering.
+This allows for arbitrary use of variables in TEMPLATE FILES, (e.g. `overrides/*.html`).
+As opposed to `macro_hooks.py` which injects variables into macro rendering (e.g. `docs/*.md`).
+If this is confusing, ask Cal to explain.
+"""
+
 import linkcheckmd as lc
 import proselint as pl
 import glob
